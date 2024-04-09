09db7bdd8241d9172fa4ba34cebb5e096389e083
Gabriellluna <glunamaia@gmail.com>
2024-04-09 15:54:14 -0300

    Alterações em alguns exercícios : Exercícios 3 e 4

--- 01-intro-python/02-funcoes/ex01.py
+++ 01-intro-python/02-funcoes/ex01.py
@@ -29,15 +29,52 @@ for i in range(1,11):
     
 #Exercício 3
 frase = input("Digite algo: ")
-frase2 = frase.strip()
-print(frase2)
-for char in frase:
-    print(char)
-print(type(frase))
-print(type(frase2))
+frase2 = frase.lower()
+vogais = 0
+consoantes = 0
 
+def contador(frase):
+    vogal = ('a', 'e', 'i', 'o', 'u')
+    consoante = ('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z')
+    vogais = 0
+    consoantes = 0
+    for char in frase:
+        if char in vogal:
+           vogais = vogais + 1
+        elif char in consoante:
+            consoantes = consoantes + 1
+    return f"O número de consoantes da frase é: {consoantes}. O de vogais é: {vogais}"          
+
+print(contador(frase2))
 
 #Exercício 4
-voto = int(input("Insira o número do candidato: "))
-i = 1
-#for i < 50:
+candidatos = {
+                "Bolsonaro - 22" : [],
+                "Lula - 13" : [],
+                "Bob Marley - 69" : []
+            }
+print("antes: ",candidatos)
+def votacao(candidatos):
+    i = 1
+    while i < 11:
+        voto = int(input("Insira o número do candidato: "))
+        match voto:
+            case 22:
+                candidatos["Bolsonaro - 22"].append(i)
+            case 13:
+                candidatos["Lula - 13"].append(i)
+            case 69:
+                candidatos["Bob Marley - 69"].append(i)
+            case _:
+                print("Número inválido")
+        i = i + 1        
+                
+    return f"depois: {candidatos}"
+       
+def eleito(candidatos):
+    for candidato, votos in candidatos.items():
+        numero_de_votos = len(votos)
+        print(f"{candidato}: {numero_de_votos} votos")            
+
+print(votacao(candidatos))
+eleito(candidatos)
